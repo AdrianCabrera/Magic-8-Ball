f7ec7650488763bfb1c5aa369150df01f576ed9d
Adrian Cabrera <11042779+AdrianCabrera@users.noreply.github.com>
2019-09-12 20:17:54 +0100

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Adrian Cabrera on 22/08/2019.
+//  Copyright © 2019 Adrian Cabrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var ballArray = [
+        "ball1",
+        "ball2",
+        "ball3",
+        "ball4",
+        "ball5"
+    ];
+    
+    var randomBallNumber: Int = 0;
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        newBallImage()
+    }
+
+    @IBAction func askButtonPressed(_ sender: Any) {
+        newBallImage()
+    }
+    
+    func newBallImage(){
+        randomBallNumber = Int.random(in: 0...4)
+        
+        imageView.image = UIImage(named: ballArray[randomBallNumber]);
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+    
+}
+
